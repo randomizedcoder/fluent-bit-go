2f354d21219a4d3fb3dce2993f45baae1f2b34fb
dave.seddon <seddon@siden.io>
2023-07-25 14:56:09 -0700

    dave2

--- output/decoder_test.go
+++ output/decoder_test.go
@@ -18,23 +18,32 @@
 package output
 
 import (
+	"encoding/binary"
+	"fmt"
+	"log"
+	"os"
 	"reflect"
 	"testing"
+	"time"
 	"unsafe"
 )
 
-// dummyRecord should be byte Array, not Slice to be able to Cast c array.
-var dummyRecord [29]byte = [29]byte{0x92, /* fix array 2 */
-	0xd7, 0x00, 0x5e, 0xa9, 0x17, 0xe0, 0x00, 0x00, 0x00, 0x00, /* 2020/04/29 06:00:00*/
-	0x82,                                           /* fix map 2*/
-	0xa7, 0x63, 0x6f, 0x6e, 0x70, 0x61, 0x63, 0x74, /* fix str 7 "compact" */
-	0xc3,                                     /* true */
-	0xa6, 0x73, 0x63, 0x68, 0x65, 0x6d, 0x61, /* fix str 6 "schema" */
-	0x01, /* fix int 1 */
-}
+const (
+	debugLevel = 11
 
-func TestGetRecord(t *testing.T) {
-	dec := NewDecoder(unsafe.Pointer(&dummyRecord), len(dummyRecord))
+	newTestDataFile = "./testdata/data.example"
+)
+
+func TestGetRecordNew(t *testing.T) {
+
+	dummyRecordNew, err := os.ReadFile(newTestDataFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	arrayPtr := (*[14140]byte)(dummyRecordNew)
+
+	dec := NewDecoder(unsafe.Pointer(arrayPtr), len(dummyRecordNew))
 	if dec == nil {
 		t.Fatal("dec is nil")
 	}
@@ -45,7 +54,7 @@ func TestGetRecord(t *testing.T) {
 	}
 
 	// test timestamp
-	ts, ok := timestamp.(FLBTime)
+	ts, ok := timestamp.(time.Time)
 	if !ok {
 		t.Fatalf("cast error. Type is %s", reflect.TypeOf(timestamp))
 	}
@@ -62,4 +71,48 @@ func TestGetRecord(t *testing.T) {
 	if v != 1 {
 		t.Errorf(`record["schema"] is not 1 %d`, v)
 	}
+}
+
+// extractTimeStamp extract the timestamp out of the MsgPack data for verification
+func extractTimeStamp(data []byte) (timestamp time.Time) {
+
+	// Manually find the time.
+	//
+	// ( Possibly there's a bug here https://github.com/fluent/fluent-bit-go/blob/master/output/decoder.go#L63, where it should be 1 not 0)
+	//
+	// The first headers of the data coming from Fluentbit looks like this
+	//	das@t:~/Downloads/siden-edge-node/deployments/local/fluentbit/db$ ~/Downloads/msgpack-inspect ./data
+	// ---
+	// - format: "array32"
+	//   header: "0xdd"
+	//   length: 2
+	//   children:
+	//     - format: "array32"
+	//       header: "0xdd"
+	//       length: 2
+	//       children:
+	//         - format: "fixext8"
+	//           header: "0xd7"
+	//           exttype: 0
+	//           length: 8
+	//           data: "0x64a46baa019bfcc0"
+	//         - format: "fixmap"
+	//           header: "0x80"
+	//           length: 0
+	//           children: []
+	// Array32 is 5 bytes long, and the fixext8 has x2 byte header, so that's where the time field starts
+	// fixext8 has 8 bytes of data
+	// Therefore, time bits start at: 5 + 5 + 2 = 12
+	// Time bits are 8 long, so 12 + 8 = 20
+	// See also: https://github.com/msgpack/msgpack/blob/master/spec.md
+	//
+	timeEightBytes := data[12:20] // extract bytes 12 through 20
+	sec := binary.BigEndian.Uint32(timeEightBytes)
+	usec := binary.BigEndian.Uint32(timeEightBytes[4:])
+	timestamp = time.Unix(int64(sec), int64(usec))
+	if debugLevel > 10 {
+		fmt.Println("timestamp:", timestamp.Format(time.RFC850))
+	}
+
+	return timestamp
 }
